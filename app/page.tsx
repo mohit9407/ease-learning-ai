cb16eb286aead536f155c8a69554efa143990986
Mohit Tailor <taylormohit9997@gmail.com>
2025-08-23 04:47:32 +0530

    init setup with shardcn is done

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className="text-2xl underline"> Heading </h1>
+      Page
+      <Button>Let's go</Button>
+    </div>
+  )
+}
+
+export default Page
